c0ea04112d89be3e7d85f9c8b052fedbbf314df5
Danilo Valério da Silva <danilo_vs@hotmail.com>
2022-06-10 19:17:04 -0300

    setup colors blackground para pecas brancas e pretas do jogo

--- src/main/java/UI.java
+++ src/main/java/UI.java
@@ -1,6 +1,20 @@
+import xadrez.Color;
 import xadrez.PecaXadrez;
 
+
+
 public class UI {
+
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+
+    public static final String ANSI_YELLOW_BACKGROUND = "\u001B[42m";
+    public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
     public static void printBoard(PecaXadrez[][] pecas){
         for (int i = 0; i < pecas.length; i++) {
             System.out.printf((8 - i) + " ");
@@ -17,7 +31,11 @@ public class UI {
         if (peca == null) {
             System.out.print("-");
         } else {
-            System.out.printf(peca.toString());
+            if (peca.getColor().equals(Color.BRANCO)) {
+                System.out.print(ANSI_WHITE_BACKGROUND + peca.toString() + ANSI_RESET);
+            } else {
+                System.out.print(ANSI_WHITE_BACKGROUND + ANSI_BLACK + peca.toString() + ANSI_RESET);
+            }
         }
         System.out.printf(" ");
     }
