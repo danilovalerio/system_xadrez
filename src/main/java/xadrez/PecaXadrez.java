a778aacebab83a01ba95a7eaa40a030b5f1aea2e
Danilo Valério da Silva <danilo_vs@hotmail.com>
2022-06-13 18:40:33 -0300

    verifica se a pessa e de um oponente

--- src/main/java/xadrez/PecaXadrez.java
+++ src/main/java/xadrez/PecaXadrez.java
@@ -1,8 +1,12 @@
 package xadrez;
 
 import tabuleiro.Peca;
+import tabuleiro.Posicao;
 import tabuleiro.Tabuleiro;
 
+/**
+ * Peça genérica que serve como base para representar uma peça concreta de xadrez
+ */
 public abstract class PecaXadrez extends Peca {
     private Color color;
 
@@ -13,5 +17,13 @@ public abstract class PecaXadrez extends Peca {
 
     public Color getColor() {
         return color;
+    }
+
+    /**
+     * operações disponível somente para classe e subclasses do mesmo pacote
+     */
+    protected boolean eUmaPecaDoOponente(Posicao posicao) {
+        PecaXadrez peca = (PecaXadrez) getTabuleiro().peca(posicao);
+        return peca != null && peca.getColor() != color;
     }
 }
